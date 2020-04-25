23127f8febcf4a85070f08fed2a9fc4a7597bd83
Edson de Senna Junior <edsondesennajunior@MacBook-Air-de-Edson.local>
2020-04-25 17:12:02 -0300

    Setting up OAuth 2.0

--- src/main/java/com/edson/gae_final/GaeFinalApplication.java
+++ src/main/java/com/edson/gae_final/GaeFinalApplication.java
@@ -2,7 +2,9 @@ package com.edson.gae_final;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
+@EnableResourceServer
 @SpringBootApplication
 public class GaeFinalApplication {
 
